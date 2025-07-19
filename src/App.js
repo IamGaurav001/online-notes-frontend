23dea16c207b283549a0bc5b73ef6b76f452001d
IamGaurav001 <hellogauravdubey@gmail.com>
2025-07-19 19:07:30 +0530

    Done with Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import CreateNotes from "./pages/CreateNotes";
+import AllNotes from "./pages/AllNotes";
+import UpdateNotes from "./pages/UpdateNotes";
+import Community from "./pages/Community";
 
 function App() {
+       
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create" element={<CreateNotes />} />
+          <Route path="/notes" element={<AllNotes />} />
+          <Route path="/update/:id" element={<UpdateNotes />} />
+          <Route path="/community" element={<Community />} />
+        </Routes>
+    </Router>
   );
 }
 
